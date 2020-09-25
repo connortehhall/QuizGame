95353117a15959c16d4422b601f4d06e6c992633
Derpish1 <connor.tehhall@hotmail.co.nz>
2020-09-25 17:27:43 +1200

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Connor Teh-Hall on 25/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
