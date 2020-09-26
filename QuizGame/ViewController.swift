36e8dc30533905793848985f85e0b418077995f7
Derpish1 <connor.tehhall@hotmail.co.nz>
2020-09-26 16:54:04 +1200

    init

--- QuizGame/ViewController.swift
+++ QuizGame/ViewController.swift
@@ -5,15 +5,22 @@
 //  Created by Connor Teh-Hall on 25/09/20.
 //
 
+/*
+ things we need
+ - menu screen
+ - game screen
+ - answer object
+ - question objec
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func startGame() {
+        // do something
+    }
 }
-
