a21662b67831f455880ede8fe08d65a1c6ab85fc
connortehhall <connor.tehhall@hotmail.co.nz>
2020-09-28 12:45:37 +1300

    added button and additional storyboard

---
+++ QuizGame/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  QuizGame
+//
+//  Created by Connor Teh-Hall on 28/09/20.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
